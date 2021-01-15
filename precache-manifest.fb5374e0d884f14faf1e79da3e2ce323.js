4da574d9209d717b9376ced967ca1005f78fb907
URpineapple <zhang.te@usky.neu.edu>
2021-01-14 19:15:32 -0500

    Updates

---
+++ precache-manifest.fb5374e0d884f14faf1e79da3e2ce323.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b50afa13712b4bb68fd2a649476ee07",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ddc05b02f23a85b5e1ae",
+    "url": "/portfolio/static/css/main.6f42e4dc.chunk.css"
+  },
+  {
+    "revision": "79227af7bbdce587bc45",
+    "url": "/portfolio/static/js/2.d1a4b217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d1a4b217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc05b02f23a85b5e1ae",
+    "url": "/portfolio/static/js/main.c0811a24.chunk.js"
+  },
+  {
+    "revision": "175d9491cb88751515ea",
+    "url": "/portfolio/static/js/runtime-main.ae8060f7.js"
+  },
+  {
+    "revision": "7d0547c98cb387bb1707c7f2b800c23e",
+    "url": "/portfolio/static/media/QRcode.7d0547c9.jpg"
+  },
+  {
+    "revision": "b3dcd48e8671f89983a93f284e9cbe8d",
+    "url": "/portfolio/static/media/musicmaster.b3dcd48e.jpg"
+  },
+  {
+    "revision": "a92a04ede1a467d08350256245ba6856",
+    "url": "/portfolio/static/media/reminder.a92a04ed.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.311347105dd5b564452562d459e4438c.js"
+  "/portfolio/precache-manifest.fb5374e0d884f14faf1e79da3e2ce323.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
