4dc3d1b123d7b6613c20f48ef41d27fc5d47c28a
URpineapple <zhang.te@usky.neu.edu>
2021-01-14 19:13:49 -0500

    Updates

---
+++ precache-manifest.311347105dd5b564452562d459e4438c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ca5615a404b5141dc97c03d6562199",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "985164d024f379b02590",
+    "url": "/portfolio-website/static/css/main.6f42e4dc.chunk.css"
+  },
+  {
+    "revision": "79227af7bbdce587bc45",
+    "url": "/portfolio-website/static/js/2.d1a4b217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.d1a4b217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985164d024f379b02590",
+    "url": "/portfolio-website/static/js/main.9a18b129.chunk.js"
+  },
+  {
+    "revision": "eaa381b3df653ed94490",
+    "url": "/portfolio-website/static/js/runtime-main.643e02a2.js"
+  },
+  {
+    "revision": "7d0547c98cb387bb1707c7f2b800c23e",
+    "url": "/portfolio-website/static/media/QRcode.7d0547c9.jpg"
+  },
+  {
+    "revision": "b3dcd48e8671f89983a93f284e9cbe8d",
+    "url": "/portfolio-website/static/media/musicmaster.b3dcd48e.jpg"
+  },
+  {
+    "revision": "a92a04ede1a467d08350256245ba6856",
+    "url": "/portfolio-website/static/media/reminder.a92a04ed.jpg"
+  }
+]);
